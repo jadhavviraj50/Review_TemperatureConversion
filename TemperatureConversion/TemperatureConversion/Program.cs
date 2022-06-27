6c8992fef816e4431b1ed1b6e6d765c89d90e715
Viraj <jadhavviraj50@gmail.com>
2022-06-27 10:37:55 +0530

    [Viraj]-Added Review of Temperature Conversion File

---
+++ TemperatureConversion/TemperatureConversion/Program.cs
@@ -0,0 +1,22 @@
+﻿using Temperatureconversion;
+
+Console.WriteLine("Welcome to temprature Conversion!");
+TemperatureCel cel = new();
+
+while (true)
+{
+    Console.WriteLine("Options : \n1)Celsius To Fahrenheit\n2)Fahrenheit to Celsius");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            cel.TemCel();
+            break;
+        case 2:
+            cel.CelTemp();
+            break;
+        default:
+            Console.WriteLine("Please choose Correct Option");
+            break;
+    }
+}
---
+++ TemperatureConversion/TemperatureConversion/TemperatureConversion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperatureconversion
+{
+    public class TemperatureCel
+    {
+        double Celsius;
+        double fahrenheit;
+        public void TemCel()
+        {
+            Console.WriteLine("Enter a Input to convert in Fahrenheit");
+            double Celsius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Celsius: " + Celsius);
+            fahrenheit = (Celsius * 9) / 5 + 32;
+            Console.WriteLine("Fahrenheit: " + fahrenheit);
+            Console.ReadLine();
+
+        }
+        public void CelTemp()
+        {
+
+            Console.WriteLine("Enter a Input to convert in Celsius");
+            double Celsius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("FeHrenheit: " + fahrenheit);
+            Celsius = (fahrenheit - 32) * 5 / 9;
+            Console.WriteLine("Celsius: " + Celsius);
+            Console.ReadLine();
+        }
+    }
+}
